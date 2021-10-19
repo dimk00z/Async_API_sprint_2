500c3f351a40f6b82dd01e74b4e4f553a9624b24
Dimk00z <dimk00z@gmail.com>
2021-10-19 13:53:07 +0300

    Fix settings

--- test/functional/settings.py
+++ test/functional/settings.py
@@ -2,18 +2,8 @@ from pydantic import Field, BaseSettings
 from pydantic.error_wrappers import ValidationError
 
 
-class ElasticSettings(BaseSettings):
+class TestSettings(BaseSettings):
     es_host: str = Field("http://127.0.0.1:9200", env="ELASTIC_HOST")
-
-
-class RedisSettings(BaseSettings):
-    host: str = Field("127.0.0.1", env="REDIS_HOST")
-    port: str = Field("6379", env="REDIS_PORT")
-
-
-def load_test_settings() -> tuple[ElasticSettings, RedisSettings]:
-    try:
-        return (ElasticSettings(), RedisSettings())
-    except ValidationError:
-        logging.error("Could load settings from enviromentals or .env")
-        raise SystemExit
+    redis_host: str = Field("127.0.0.1", env="REDIS_HOST")
+    redis_port: str = Field("6379", env="REDIS_PORT")
+    async_api_host: str = Field("http://127.0.0.1:8000/api/v1", env="ASYNC_API_HOST")
