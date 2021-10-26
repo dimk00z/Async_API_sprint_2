ea201dad3e7ea2260a541e8e8f6beb28355d7c35
Arseny Egorov <egoroff-ars@yandex.ru>
2021-10-26 10:27:24 +0300

    04 functional test infra (#12)
    * Add indexes dump
    
    * Infra for tests. Data ingestion for Elastic from files. Flush Redis.
    
    * Add main for connections.py
    
    * Add redis version
    
    * Remove path from connections.py
    
    Co-authored-by: Dimk00z

---
+++ tests/functional/utils/connections.py
@@ -0,0 +1,52 @@
+import asyncio
+import logging
+
+import backoff
+import aioredis
+import settings
+from elasticsearch import AsyncElasticsearch
+
+
+def backoff_handler(details):
+    logging.error(
+        "Backing off {wait:0.1f} seconds after {tries} tries "
+        "calling function {target} with args {args} and kwargs "
+        "{kwargs}".format(**details)
+    )
+
+
+@backoff.on_exception(
+    backoff.expo,
+    (ConnectionError,),
+    on_backoff=backoff_handler,
+    max_time=settings.CONNECTIONS_MAX_TIME,
+)
+async def redis_connect(*, host: str, port: int) -> aioredis.Redis:
+    redis = aioredis.from_url(f"redis://{host}:{port}")
+    pong = await redis.ping()
+    if not pong:
+        raise ConnectionError("Connection failed")
+    return redis
+
+
+@backoff.on_exception(
+    backoff.expo,
+    (ConnectionError,),
+    on_backoff=backoff_handler,
+    max_time=settings.CONNECTIONS_MAX_TIME,
+)
+async def elastic_connect(*, host: str) -> AsyncElasticsearch:
+    elastic = AsyncElasticsearch(hosts=host, verify_certs=True)
+    if not await elastic.ping():
+        raise ConnectionError("Connection failed")
+    return elastic
+
+
+async def main():
+    "Waiters for Elasticsearch and REDIS"
+    await elastic_connect(host=settings.es_host)
+    await redis_connect(host=settings.redis_host, port=settings.redis_port)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
