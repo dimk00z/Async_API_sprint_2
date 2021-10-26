ad5c1c34a18e5caa03672847e88cdcd72655c9e9
Dmitry <dimk00z@gmail.com>
2021-10-26 11:37:45 +0300

    05 functional test film (#13)
    * Добавлены тесты для фильмов
    * Вынесены общие фикстуры
    #6

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,7 +1,19 @@
+import asyncio
+from dataclasses import dataclass
+
 import pytest
+import aiohttp
 from settings import Settings
+from multidict import CIMultiDictProxy
 from utils.setup import redis_setup, elastic_setup
 from utils.connections import redis_connect, elastic_connect
+
+
+@dataclass
+class HTTPResponse:
+    body: dict
+    headers: CIMultiDictProxy[str]
+    status: int
 
 
 @pytest.fixture(scope="session")
@@ -28,4 +40,36 @@ async def redis_client(settings):
     await redis_setup(redis_client=client)
 
     yield client
-    await client.close()
+
+
+@pytest.fixture(scope="session")
+async def session():
+    session = aiohttp.ClientSession()
+    yield session
+    await session.close()
+
+
+@pytest.fixture(scope="session")
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture
+def get_request(session):
+    async def inner(method: str, params: dict = None) -> HTTPResponse:
+        params = params or {}
+        async_api_host = Settings().async_api_host
+        # export ASYNC_API_HOST="http://178.154.213.182:8000/api/v1" - для проверки на живом
+        # python -m pytest -vv
+        url = f"{async_api_host}{method}"
+        async with session.get(url, params=params) as response:
+            return HTTPResponse(
+                body=await response.json(),
+                headers=response.headers,
+                status=response.status,
+            )
+
+    return inner
---
+++ tests/functional/testdata/films_test_data.py
@@ -0,0 +1,23 @@
+films_id_params = [
+    ("2a090dde-f688-46fe-a9f4-b781a985275e", "Star Wars: Knights of the Old Republic", 9.6, 200),
+    ("fd78a0e5-d4ec-435e-8994-4ccbdfc4e60b", "Lone Star Restoration", 8.7, 200),
+    ("3f8873be-f6b1-4f3f-8a01-873924659851", "Justin Bieber: A Star Was Born", 1, 200),
+    ("3f8873be-f6b1-4f3f-8000-873924659851", None, None, 404),
+]
+films_sorting_params = ["imdb_rating", "-imdb_rating", "wrong_sort"]
+
+films_pages_params = [(1, 200), (3, 200), (10, 200), (-10, 200), (1000, 404)]
+
+films_len_pages_params = [(1, 1, 200), (10, 10, 200), (100, 100, 200), (100000, 1, 404)]
+
+films_genres = [
+    ("b92ef010-5e4c-4fd0-99d6-41b6456272cd", 200),
+    ("120a21cf-9097-479e-904a-13dd7198c1dd", 200),
+    ("wrong_uuid", 404),
+]
+
+films_query_params = [
+    ("Rebellion", 10, 200),
+    ("Foxx", 1, 200),
+    ("qwerty", 1, 404),
+]
