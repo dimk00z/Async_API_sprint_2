b9ec6f474889581d48694a67092170494367ccfb
Dimk00z <dimk00z@gmail.com>
2021-10-26 11:34:19 +0300

    Fix testdata naming

--- tests/functional/src/test_films.py
+++ tests/functional/src/test_films.py
@@ -1,15 +1,15 @@
 import pytest
 from testdata.films_test_data import (
-    film_genres,
-    film_id_params,
-    film_query_params,
+    films_genres,
+    films_id_params,
     films_pages_params,
+    films_query_params,
     films_sorting_params,
     films_len_pages_params,
 )
 
 
-@pytest.mark.parametrize("film_uuid, title, imdb_rating, expected_response_status", film_id_params)
+@pytest.mark.parametrize("film_uuid, title, imdb_rating, expected_response_status", films_id_params)
 @pytest.mark.asyncio
 async def test_film_by_id(get_request, film_uuid, title, imdb_rating, expected_response_status):
     response = await get_request(f"/film/{film_uuid}")
@@ -40,14 +40,14 @@ async def test_film_len_pages(get_request, page_number, page_len, expected_respo
     assert len(response.body) == page_len
 
 
-@pytest.mark.parametrize("genre_uuid, expected_response_status", film_genres)
+@pytest.mark.parametrize("genre_uuid, expected_response_status", films_genres)
 @pytest.mark.asyncio
 async def test_film_pages(get_request, genre_uuid, expected_response_status):
     response = await get_request(f"/film", {"filter[genre]": genre_uuid})
     assert response.status == expected_response_status
 
 
-@pytest.mark.parametrize("query, page_len, expected_response_status", film_query_params)
+@pytest.mark.parametrize("query, page_len, expected_response_status", films_query_params)
 @pytest.mark.asyncio
 async def test_film_search(get_request, query, page_len, expected_response_status):
     response = await get_request(f"/film/search", {"query": query})
--- tests/functional/testdata/films_test_data.py
+++ tests/functional/testdata/films_test_data.py
@@ -1,4 +1,4 @@
-film_id_params = [
+films_id_params = [
     ("2a090dde-f688-46fe-a9f4-b781a985275e", "Star Wars: Knights of the Old Republic", 9.6, 200),
     ("fd78a0e5-d4ec-435e-8994-4ccbdfc4e60b", "Lone Star Restoration", 8.7, 200),
     ("3f8873be-f6b1-4f3f-8a01-873924659851", "Justin Bieber: A Star Was Born", 1, 200),
@@ -10,13 +10,13 @@ films_pages_params = [(1, 200), (3, 200), (10, 200), (-10, 200), (1000, 404)]
 
 films_len_pages_params = [(1, 1, 200), (10, 10, 200), (100, 100, 200), (100000, 1, 404)]
 
-film_genres = [
+films_genres = [
     ("b92ef010-5e4c-4fd0-99d6-41b6456272cd", 200),
     ("120a21cf-9097-479e-904a-13dd7198c1dd", 200),
     ("wrong_uuid", 404),
 ]
 
-film_query_params = [
+films_query_params = [
     ("Rebellion", 10, 200),
     ("Foxx", 1, 200),
     ("qwerty", 1, 404),
