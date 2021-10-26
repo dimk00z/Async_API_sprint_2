7977d983f6f024e95ae8901e3e1a9fb349aa496c
Dimk00z <dimk00z@gmail.com>
2021-10-26 09:59:05 +0300

    Add main for waitings

--- tests/functional/utils/connections.py
+++ tests/functional/utils/connections.py
@@ -1,9 +1,17 @@
+import os
+import sys
+import asyncio
+import inspect
 import logging
 
 import backoff
 import aioredis
-from settings import CONNECTIONS_MAX_TIME
+import settings
 from elasticsearch import AsyncElasticsearch
+
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
 
 
 def backoff_handler(details):
@@ -18,7 +26,7 @@ def backoff_handler(details):
     backoff.expo,
     (ConnectionError,),
     on_backoff=backoff_handler,
-    max_time=CONNECTIONS_MAX_TIME,
+    max_time=settings.CONNECTIONS_MAX_TIME,
 )
 async def redis_connect(*, host: str, port: int) -> aioredis.Redis:
     redis = aioredis.from_url(f"redis://{host}:{port}")
@@ -32,10 +40,20 @@ async def redis_connect(*, host: str, port: int) -> aioredis.Redis:
     backoff.expo,
     (ConnectionError,),
     on_backoff=backoff_handler,
-    max_time=CONNECTIONS_MAX_TIME,
+    max_time=settings.CONNECTIONS_MAX_TIME,
 )
 async def elastic_connect(*, host: str) -> AsyncElasticsearch:
     elastic = AsyncElasticsearch(hosts=host, verify_certs=True)
     if not await elastic.ping():
         raise ConnectionError("Connection failed")
     return elastic
+
+
+async def main():
+    "Waiters for Elasticsearch and REDIS"
+    await elastic_connect(host=settings.es_host)
+    await redis_connect(host=settings.redis_host, port=settings.redis_port)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
