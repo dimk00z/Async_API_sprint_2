eb329f07bc221a80c904c6a621e2f7cd3fbe02e4
Dimk00z <dimk00z@gmail.com>
2021-10-28 15:24:19 +0300

    Add film docs

--- async_api/src/api/v1/film.py
+++ async_api/src/api/v1/film.py
@@ -28,9 +28,7 @@ async def get_films(
     )
 
     if not films:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail="not one film found"
-        )
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="not one film found")
     if "error" in films:
         return HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=films["error"])
     return [
@@ -43,31 +41,55 @@ async def get_films(
     ]
 
 
-@router.get("/")
+@router.get(
+    "/",
+    summary="Список кинопроизведений",
+)
 async def films_list(
     film_service: FilmService = Depends(get_film_service),
     filter_genre: Optional[str] = Query(None, alias="filter[genre]"),
-    sort: Optional[str] = Query(None, regex="^-?[a-zA-Z_]+$"),
+    sort_: Optional[str] = Query(None, regex="^-?[a-zA-Z_]+$", alias="sort"),
     page_number: int = Query(1, alias="page[number]"),
     page_size: int = Query(50, alias="page[size]"),
 ) -> list[dict]:
+    """
+    Для вывода доступны следующие параметры:
+    - **filter[genre]**: сортировка по uuid жанра"
+    - **sort**: сортировка по рейтингу -imdb_rating/imdb_rating
+    - **page[number]**: запрашиваемая страница
+    - **page[size]**: размер страницы
+
+    """
     return await get_films(
         film_service=film_service,
-        sort=sort,
+        sort=sort_,
         filter_genre=filter_genre,
         page_number=page_number,
         page_size=page_size,
     )
 
 
-@router.get("/search")
+@router.get(
+    "/search",
+    summary="Поиск кинопроизведений",
+)
 async def films_search(
     film_service: FilmService = Depends(get_film_service),
-    sort_: Optional[str] = Query(None, regex="^-?[a-zA-Z_]+$", alias="page[size]"),
+    sort_: Optional[str] = Query(None, regex="^-?[a-zA-Z_]+$", alias="sort"),
     page_number_: int = Query(1, alias="page[number]"),
     page_size_: int = Query(50, alias="page[size]"),
-    query_: Optional[str] = Query(None, title="Поисковая строка", alias="query"),
+    query_: Optional[str] = Query(
+        None,
+        alias="query",
+    ),
 ) -> list[dict]:
+    """
+    Для поиска доступны следующие параметры:
+    - **query**: строка для поиска
+    - **sort**: сортировка по рейтингу -imdb_rating/imdb_rating
+    - **page[number]**: запрашиваемая страница
+    - **page[size]**: размер страницы
+    """
     return await get_films(
         film_service=film_service,
         sort=sort_,
@@ -77,11 +99,20 @@ async def films_search(
     )
 
 
-@router.get("/{film_uuid}", response_model=Film)
+@router.get(
+    "/{film_uuid}",
+    response_model=Film,
+    summary="Подробные данные о фильме",
+)
 async def film_details(
     film_uuid: UUID,
     film_service: FilmService = Depends(get_film_service),
 ) -> Film:
+    """
+    Вывод информации о фильме по uuid:
+
+    - **film_uuid**: требуемое поле
+    """
     film = await film_service.get_by_uuid(uuid=film_uuid)
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
