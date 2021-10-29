d7a4cb0726a859bbbd579376de58a93c29db68c8
D. Murdashev <d.murdashev@outlook.com>
2021-10-29 15:21:15 +1000

    Add documentation in genre

--- async_api/src/api/v1/genre.py
+++ async_api/src/api/v1/genre.py
@@ -27,12 +27,19 @@ async def get_genres(
     return genres
 
 
-@router.get("/")
+@router.get(
+    "/",
+    summary="Список жанров")
 async def genres_list(
     genres_service: GenreService = Depends(get_genre_service),
     page_number: int = Query(1, alias="page[number]"),
     page_size: int = Query(50, alias="page[size]"),
 ) -> list[Genre]:
+    """
+    Возможно задать следующие параметры:
+    - **page[number]**: запрашиваемая страница
+    - **page[size]**: размер страницы
+    """
     return await get_genres(
         genres_service=genres_service,
         page_number=page_number,
@@ -40,11 +47,17 @@ async def genres_list(
     )
 
 
-@router.get("/{genre_uuid}")
+@router.get(
+    "/{genre_uuid}",
+    summary="Поиск жанра по uuid")
 async def genre_details(
     genre_uuid: UUID,
     genre_service: GenreService = Depends(get_genre_service),
 ) -> Optional[Genre]:
+    """
+    Обязательный параметр:
+    - **genre_uuid**: uuid жанра
+    """
     genre = await genre_service.get_by_uuid(uuid=genre_uuid)
     if genre:
         return genre
