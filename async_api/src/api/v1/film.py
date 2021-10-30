e1d625236db6592894f4d5dac75d5826e9a06fa6
Dimk00z <dimk00z@gmail.com>
2021-10-30 19:47:41 +0300

    Fix endpoints

--- async_api/src/api/v1/film.py
+++ async_api/src/api/v1/film.py
@@ -12,27 +12,28 @@ persons = Optional[list[dict[str, str]]]
 
 
 async def get_films(
-    film_service: FilmService = Depends(get_film_service),
-    filter_genre: Optional[str] = "",
-    sort: Optional[str] = Query(None, regex="^-?[a-zA-Z_]+$"),
-    page_number: int = 1,
-    page_size: int = 50,
-    query: Optional[str] = "",
+    film_service: FilmService,
+    **end_point_params
+    # filter_genre: Optional[str] = "",
+    # sort: Optional[str] = Query(None, regex="^-?[a-zA-Z_]+$"),
+    # page_number: int = 1,
+    # page_size: int = 50,
+    # query: Optional[str] = "",
 ):
     films = await film_service.get_films(
-        sort=sort,
-        filter_genre=filter_genre if filter_genre is not Query(None) else "",
-        page_number=page_number,
-        page_size=page_size,
-        query=str(query),
+        **{key: value for key, value in end_point_params.items() if value}
+        # sort=sort,
+        # filter_genre=filter_genre if filter_genre is not Query(None) else "",
+        # page_number=page_number,
+        # page_size=page_size,
+        # query=str(query),
     )
 
     if not films:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail="not one film found"
         )
-    if "error" in films:
-        return HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=films["error"])
+
     return [
         {
             "uuid": film.uuid,
@@ -50,9 +51,11 @@ async def get_films(
 async def films_list(
     film_service: FilmService = Depends(get_film_service),
     filter_genre: Optional[str] = Query(None, alias="filter[genre]"),
-    sort_: Optional[str] = Query(None, regex="^-?[a-zA-Z_]+$", alias="sort"),
-    page_number: int = Query(1, alias="page[number]"),
-    page_size: int = Query(50, alias="page[size]"),
+    sort: Optional[str] = Query(
+        default="-imdb_rating", regex="^-?[a-zA-Z_]+$", alias="sort"
+    ),
+    page_number: int = Query(default=0, alias="page[number]"),
+    page_size: int = Query(default=50, alias="page[size]"),
 ) -> list[dict]:
     """
     Для вывода доступны следующие параметры:
@@ -64,10 +67,10 @@ async def films_list(
     """
     return await get_films(
         film_service=film_service,
-        sort=sort_,
+        sort=sort,
         filter_genre=filter_genre,
-        page_number=page_number,
         page_size=page_size,
+        page_number=page_number,
     )
 
 
@@ -77,10 +80,10 @@ async def films_list(
 )
 async def films_search(
     film_service: FilmService = Depends(get_film_service),
-    sort_: Optional[str] = Query(None, regex="^-?[a-zA-Z_]+$", alias="sort"),
-    page_number_: int = Query(1, alias="page[number]"),
-    page_size_: int = Query(50, alias="page[size]"),
-    query_: Optional[str] = Query(
+    sort: Optional[str] = Query(None, regex="^-?[a-zA-Z_]+$", alias="sort"),
+    page_number: int = Query(0, alias="page[number]"),
+    page_size: int = Query(50, alias="page[size]"),
+    query: Optional[str] = Query(
         None,
         alias="query",
     ),
@@ -94,10 +97,10 @@ async def films_search(
     """
     return await get_films(
         film_service=film_service,
-        sort=sort_,
-        query=query_,
-        page_number=page_number_,
-        page_size=page_size_,
+        sort=sort,
+        query=query,
+        page_number=page_number,
+        page_size=page_size,
     )
 
 
