d56afc0893b6fb4bc800e56a1f3b282f55ec3a72
Dimk00z <dimk00z@gmail.com>
2021-11-01 09:25:05 +0300

    Add blank lines for test

--- tests/functional/src/test_films.py
+++ tests/functional/src/test_films.py
@@ -22,9 +22,7 @@ async def test_film_by_uuid(
 
 
 @pytest.mark.parametrize("params,expected_status,expected_body", FILM_SEARCH_DATA)
-async def test_film_search(
-    get_request, test_case_helper, params, expected_status, expected_body
-):
+async def test_film_search(get_request, test_case_helper, params, expected_status, expected_body):
     response = await get_request("/film/search", params=params)
 
     assert response.status == expected_status
@@ -36,30 +34,25 @@ async def test_film_imdb_sorting(
     get_request, test_case_helper, sorting, expected_body, expected_status
 ):
     response = await get_request(f"/film", {"sort": sorting, "page[size]": 2})
+
     assert response.status == expected_status
     test_case_helper.assertEqual(response.body, expected_body)
 
 
-@pytest.mark.parametrize(
-    "page_number, expected_response_status, expected_body", FILMS_PAGES_PARAMS
-)
+@pytest.mark.parametrize("page_number, expected_response_status, expected_body", FILMS_PAGES_PARAMS)
 async def test_film_pages(
     get_request, test_case_helper, page_number, expected_response_status, expected_body
 ):
-    response = await get_request(
-        f"/film", {"page[number]": page_number, "page[size]": 2}
-    )
+    response = await get_request(f"/film", {"page[number]": page_number, "page[size]": 2})
+
     assert response.status == expected_response_status
     test_case_helper.assertEqual(response.body, expected_body)
 
 
-@pytest.mark.parametrize(
-    "page_number, page_len, expected_response_status", FILMS_LEN_PAGES_PARAMS
-)
-async def test_film_len_pages(
-    get_request, page_number, page_len, expected_response_status
-):
+@pytest.mark.parametrize("page_number, page_len, expected_response_status", FILMS_LEN_PAGES_PARAMS)
+async def test_film_len_pages(get_request, page_number, page_len, expected_response_status):
     response = await get_request(f"/film", {"page[size]": page_number})
+
     assert response.status == expected_response_status
     assert len(response.body) == page_len
 
@@ -67,4 +60,5 @@ async def test_film_len_pages(
 @pytest.mark.parametrize("genre_uuid, expected_response_status", FILMS_GENRES)
 async def test_film_genre_sort(get_request, genre_uuid, expected_response_status):
     response = await get_request(f"/film", {"filter[genre]": genre_uuid})
+
     assert response.status == expected_response_status
